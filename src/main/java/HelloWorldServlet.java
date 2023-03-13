bde9e70eaf267b8c25fe03e9220c55bba60b7abb
Andre Murray <slushymatis@gmail.com>
2023-03-13 14:29:51 -0500

    servlets with jsps

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.getWriter().println("<h1>Hello, World</h1>");
+        response.getWriter().println("<h5>Hello, World</h5>");
+        response.getWriter().println("<h2>Hello world servlet</h2>");
     }
 }
